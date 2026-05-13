4c481949309085073fc42025ddd427df0d8d8960
Ammarguellat <zahira.ammarguellat@intel.com>
2026-05-13 13:25:50 -0700

    Fix format

--- clang/lib/Sema/SemaStmt.cpp
+++ clang/lib/Sema/SemaStmt.cpp
@@ -4729,7 +4729,7 @@ buildCapturedStmtCaptureList(Sema &S, CapturedRegionScopeInfo *RSI,
         }
         VarDecl *DD = cast<VarDecl>(BD->getDecomposedDecl());
         if (!CapturedDecomposed.insert(DD).second) {
-          continue; // Skip duplicate.  
+          continue; // Skip duplicate.
         }
         CapVar = DD;
       }
