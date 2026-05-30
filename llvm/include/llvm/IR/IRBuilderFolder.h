c1b9725b82ddabb5d77ff5a12914fdb3874aff3f
Pedro Vicente <pedro.costa.vicente@tecnico.ulisboa.pt>
2026-05-30 16:53:31 +0100

    [IR] Implementation and lowering of bitinsert and bitextract This commit implements bitinsert and bitextract instructions to manipulate Byte types. These were lowered to SDAG and GlobalISel, and tests for IR and X86 were added.
    Co-authored-by: Guilherme Lopes <guilhermeglopes@tecnico.ulisboa.pt>

--- llvm/include/llvm/IR/IRBuilderFolder.h
+++ llvm/include/llvm/IR/IRBuilderFolder.h
@@ -86,6 +86,12 @@ public:
   virtual Value *FoldBitExtract(Type *Ty, Value *Src,
                                 Value *Offset) const = 0;
 
+  virtual Value *FoldBitInsert(Value *Base, Value *Val,
+                               Value *Offset) const = 0;
+
+  virtual Value *FoldBitExtract(Type *Ty, Value *Src,
+                                Value *Offset) const = 0;
+
   //===--------------------------------------------------------------------===//
   // Cast/Conversion Operators
   //===--------------------------------------------------------------------===//
