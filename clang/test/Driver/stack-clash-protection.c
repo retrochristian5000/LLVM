cf13b0244734b75c596da44f826e33245306a892
Alearner12 <siddharthtiwari155@yahoo.com>
2026-07-14 02:41:59 +0530

    [clang][test] Simplify stack clash driver checks
    Use a shared ENABLED prefix for tests that expect the driver option and implicit negative checks for disabled cases.

--- clang/test/Driver/stack-clash-protection.c
+++ clang/test/Driver/stack-clash-protection.c
@@ -1,30 +1,25 @@
-// RUN: %clang -target i386-unknown-linux -fstack-clash-protection -### %s 2>&1 | FileCheck %s -check-prefix=SCP-i386
-// RUN: %clang -target i386-unknown-linux -fno-stack-clash-protection -fstack-clash-protection -### %s 2>&1 | FileCheck %s -check-prefix=SCP-i386
-// RUN: %clang -target i386-unknown-linux -fstack-clash-protection -fno-stack-clash-protection -### %s 2>&1 | FileCheck %s -check-prefix=SCP-i386-NO
-// SCP-i386: "-fstack-clash-protection"
-// SCP-i386-NO-NOT: "-fstack-clash-protection"
+// RUN: %clang -target i386-unknown-linux -fstack-clash-protection -### %s 2>&1 | FileCheck %s --check-prefix=ENABLED
+// RUN: %clang -target i386-unknown-linux -fno-stack-clash-protection -fstack-clash-protection -### %s 2>&1 | FileCheck %s --check-prefix=ENABLED
+// RUN: %clang -target i386-unknown-linux -fstack-clash-protection -fno-stack-clash-protection -### %s 2>&1 | FileCheck %s --check-prefix=DISABLED --implicit-check-not='"-fstack-clash-protection"'
 
-// RUN: %clang -target x86_64-scei-linux -fstack-clash-protection -### %s 2>&1 | FileCheck %s -check-prefix=SCP-x86
-// RUN: %clang -target x86_64-unknown-freebsd -fstack-clash-protection -### %s 2>&1 | FileCheck %s -check-prefix=SCP-x86
-// SCP-x86: "-fstack-clash-protection"
+// RUN: %clang -target x86_64-scei-linux -fstack-clash-protection -### %s 2>&1 | FileCheck %s --check-prefix=ENABLED
+// RUN: %clang -target x86_64-unknown-freebsd -fstack-clash-protection -### %s 2>&1 | FileCheck %s --check-prefix=ENABLED
 
-// RUN: %clang -target x86_64-unknown-linux -### %s 2>&1 | FileCheck %s -check-prefix=SCP-LINUX-NO
-// SCP-LINUX-NO-NOT: "-fstack-clash-protection"
+// RUN: %clang -target x86_64-unknown-linux -### %s 2>&1 | FileCheck %s --check-prefix=DISABLED --implicit-check-not='"-fstack-clash-protection"'
 
-// RUN: %clang -target x86_64-linux-android -### %s 2>&1 | FileCheck %s -check-prefix=SCP-ANDROID
-// RUN: %clang -target i686-linux-android -### %s 2>&1 | FileCheck %s -check-prefix=SCP-ANDROID
-// RUN: %clang -target aarch64-linux-android -### %s 2>&1 | FileCheck %s -check-prefix=SCP-ANDROID
-// RUN: %clang -target riscv64-linux-android -### %s 2>&1 | FileCheck %s -check-prefix=SCP-ANDROID
-// RUN: %clang -target aarch64-linux-android -fno-stack-clash-protection -### %s 2>&1 | FileCheck %s -check-prefix=SCP-ANDROID-NO
-// RUN: %clang -target aarch64-linux-android -fno-stack-clash-protection -fstack-clash-protection -### %s 2>&1 | FileCheck %s -check-prefix=SCP-ANDROID
-// RUN: %clang -target aarch64-linux-android -fstack-clash-protection -fno-stack-clash-protection -### %s 2>&1 | FileCheck %s -check-prefix=SCP-ANDROID-NO
-// RUN: %clang -target armv7-linux-androideabi -### %s 2>&1 | FileCheck %s -check-prefix=SCP-ANDROID-ARMV7
-// SCP-ANDROID: "-fstack-clash-protection"
-// SCP-ANDROID-NO-NOT: "-fstack-clash-protection"
-// SCP-ANDROID-ARMV7-NOT: "-fstack-clash-protection"
+// RUN: %clang -target x86_64-linux-android -### %s 2>&1 | FileCheck %s --check-prefix=ENABLED
+// RUN: %clang -target i686-linux-android -### %s 2>&1 | FileCheck %s --check-prefix=ENABLED
+// RUN: %clang -target aarch64-linux-android -### %s 2>&1 | FileCheck %s --check-prefix=ENABLED
+// RUN: %clang -target riscv64-linux-android -### %s 2>&1 | FileCheck %s --check-prefix=ENABLED
+// RUN: %clang -target aarch64-linux-android -fno-stack-clash-protection -### %s 2>&1 | FileCheck %s --check-prefix=DISABLED --implicit-check-not='"-fstack-clash-protection"'
+// RUN: %clang -target aarch64-linux-android -fno-stack-clash-protection -fstack-clash-protection -### %s 2>&1 | FileCheck %s --check-prefix=ENABLED
+// RUN: %clang -target aarch64-linux-android -fstack-clash-protection -fno-stack-clash-protection -### %s 2>&1 | FileCheck %s --check-prefix=DISABLED --implicit-check-not='"-fstack-clash-protection"'
+// RUN: %clang -target armv7-linux-androideabi -### %s 2>&1 | FileCheck %s --check-prefix=DISABLED --implicit-check-not='"-fstack-clash-protection"'
 
-// RUN: %clang -target armv7k-apple-linux -fstack-clash-protection -### %s 2>&1 | FileCheck %s -check-prefix=SCP-armv7
-// SCP-armv7-NOT: "-fstack-clash-protection"
+// ENABLED: "-fstack-clash-protection"
+// DISABLED: "-cc1"
+
+// RUN: %clang -target armv7k-apple-linux -fstack-clash-protection -### %s 2>&1 | FileCheck %s --check-prefix=SCP-armv7 --implicit-check-not='"-fstack-clash-protection"'
 // SCP-armv7: argument unused during compilation: '-fstack-clash-protection'
 
 // RUN: %clang -target x86_64-unknown-linux -fstack-clash-protection -S -emit-llvm -o %t.ll %s 2>&1 | FileCheck %s -check-prefix=SCP-warn
@@ -37,10 +32,9 @@
 // SCP-ll-win64-NOT: attributes {{.*}} "probe-stack"="inline-asm"
 // SCP-ll-win64: argument unused during compilation: '-fstack-clash-protection'
 
-// RUN: %clang -target x86_64-unknown-fuchsia -fstack-clash-protection -### %s 2>&1 | FileCheck %s -check-prefix=SCP-FUCHSIA
-// RUN: %clang -target aarch64-unknown-fuchsia -fstack-clash-protection -### %s 2>&1 | FileCheck %s -check-prefix=SCP-FUCHSIA
-// RUN: %clang -target riscv64-unknown-fuchsia -fstack-clash-protection -### %s 2>&1 | FileCheck %s -check-prefix=SCP-FUCHSIA
-// SCP-FUCHSIA: "-fstack-clash-protection"
+// RUN: %clang -target x86_64-unknown-fuchsia -fstack-clash-protection -### %s 2>&1 | FileCheck %s --check-prefix=ENABLED
+// RUN: %clang -target aarch64-unknown-fuchsia -fstack-clash-protection -### %s 2>&1 | FileCheck %s --check-prefix=ENABLED
+// RUN: %clang -target riscv64-unknown-fuchsia -fstack-clash-protection -### %s 2>&1 | FileCheck %s --check-prefix=ENABLED
 
 int foo(int c) {
   int r;
