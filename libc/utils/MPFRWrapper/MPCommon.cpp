698cfb2a90ef09fb6eb354f39f430f0349d04e78
Vedant Neve <vedantneve13@gmail.com>
2026-07-16 15:57:27 +0530

    [libc][math] Optimize tgammabf16 with exact hex literals and bitwise checks
    Signed-off-by: Vedant Neve <vedantneve13@gmail.com>

--- libc/utils/MPFRWrapper/MPCommon.cpp
+++ libc/utils/MPFRWrapper/MPCommon.cpp
@@ -529,12 +529,6 @@ MPFRNumber MPFRNumber::tanh() const {
   return result;
 }
 
-MPFRNumber MPFRNumber::tgamma() const {
-  MPFRNumber result(*this);
-  mpfr_gamma(result.value, value, mpfr_rounding);
-  return result;
-}
-
 MPFRNumber MPFRNumber::tanpi() const {
   MPFRNumber result(*this);
 
@@ -579,6 +573,12 @@ MPFRNumber MPFRNumber::tanpi() const {
   mpfr_tan(result.value, value_pi.value, mpfr_rounding);
   return result;
 #endif
+}
+
+MPFRNumber MPFRNumber::tgamma() const {
+  MPFRNumber result(*this);
+  mpfr_gamma(result.value, value, mpfr_rounding);
+  return result;
 }
 
 MPFRNumber MPFRNumber::trunc() const {
