1d0b089bcdc54e9b0571c7082a8beafc9d4b1624
Pedro Vicente <pedro.costa.vicente@tecnico.ulisboa.pt>
2026-07-19 00:46:46 +0100

    small update in SDag and Globalisel

--- llvm/lib/CodeGen/GlobalISel/IRTranslator.cpp
+++ llvm/lib/CodeGen/GlobalISel/IRTranslator.cpp
@@ -3567,6 +3567,9 @@ bool IRTranslator::translateBitInsert(const User &U,
   LLT BaseTy = MRI.getType(Base);
   LLT ValTy = MRI.getType(Val);
 
+  assert(BaseTy.getSizeInBits() >= ValTy.getSizeInBits() &&
+        "bitinsert val wider than base should be rejected by verifier");
+
   // If Val is a floating-point type, bitcast it to an integer of the same
   // size so buildZExtOrTrunc can safely extend or truncate it.
   if (ValTy.isFloat()) {
