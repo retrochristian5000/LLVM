llvm/llvm-project

[LV] Fix miscompile when widening induction with loop-variant step
InductionDescriptor::isInductionPHI classifies a phi as IK_IntInduction
whenever the backedge value is formed by an add/sub of any step — even a
loop-variant one.  createWidenInductionRecipe was then calling
setOperand(1, IndDesc.getStep()), replacing the real VPlan step with the
loop-invariant IndDesc step; and ReplaceExtractsWithExitingIVValue was
computing ExitingIVValue(WideIV) = start + n_iterations * IndDesc.step,
which is wrong when the true step is variant.

Add hasVariantStepIncrement, which walks the VPlan step operand through
Add/Sub looking for a non-loop-invariant leaf via getSCEVExprForVPValue.
Guard both the setOperand clobber and the extract-replacement with
!HasVariantStepIncrement so variant-step inductions use the correct
per-lane exit value from an extractelement instead.

Fixes https://github.com/llvm/llvm-project/issues/205799.

Co-Authored-By: Claude Sonnet 4.6 <noreply@anthropic.com>

llvm/lib/Transforms/Vectorize/VPlanConstruction.cpp
@@ -615,6 +615,40 @@ static void simplifyLiveInsWithSCEV(VPlan &Plan,
 /// To make RUN_VPLAN_PASS print initial VPlan.
 static void printAfterInitialConstruction(VPlan &) {}
 
+/// Returns true if \p BackedgeVal advances \p PhiR by a loop-variant step.
+static bool hasVariantStepIncrement(VPValue *BackedgeVal, VPPhi *PhiR,
+                                    const InductionDescriptor &IndDesc,
+                                    PredicatedScalarEvolution &PSE,
+                                    Loop &OrigLoop) {
+  if (IndDesc.getKind() != InductionDescriptor::IK_IntInduction)
+    return false;
+
+  // Use m_c_Add to capture ActualStep regardless of operand order; subtraction
+  // is non-commutative so m_Sub requires PhiR on the left.
+  VPValue *ActualStep = nullptr;
+  switch (IndDesc.getInductionOpcode()) {
+  case Instruction::Add:
+    if (!match(BackedgeVal, m_c_Add(m_Specific(PhiR), m_VPValue(ActualStep))))
+      return false;
+    break;
+  case Instruction::Sub:
+    if (!match(BackedgeVal, m_Sub(m_Specific(PhiR), m_VPValue(ActualStep))))
+      return false;
+    break;
+  default:
+    return false;
+  }
+
+  const SCEV *ActualStepSCEV = vputils::getSCEVExprForVPValue(ActualStep, PSE);
+  // If the VPlan step has no computable SCEV, conservatively treat it as
+  // variant to avoid replacing it with the (potentially mismatched) IndDesc
+  // step or computing a wrong exit value.
+  if (isa<SCEVCouldNotCompute>(ActualStepSCEV))
+    return true;
+
+  return !PSE.getSE()->isLoopInvariant(ActualStepSCEV, &OrigLoop);
+}
+
 std::unique_ptr<VPlan>
 VPlanTransforms::buildVPlan0(Loop *TheLoop, LoopInfo &LI, Type *InductionTy,
                              PredicatedScalarEvolution &PSE,
@@ -686,7 +720,10 @@ createWidenInductionRecipe(PHINode *Phi, VPPhi *PhiR, VPIRValue *Start,
   // Update wide induction increments to use the same step as the corresponding
   // wide induction. This enables detecting induction increments directly in
   // VPlan and removes redundant splats.
-  if (match(BackedgeVal, m_Add(m_Specific(PhiR), m_VPValue())))
+  bool HasVariantStepIncrement =
+      hasVariantStepIncrement(BackedgeVal, PhiR, IndDesc, PSE, OrigLoop);
+  if (!HasVariantStepIncrement &&
+      match(BackedgeVal, m_Add(m_Specific(PhiR), m_VPValue())))
     BackedgeVal->getDefiningRecipe()->setOperand(1, Step);
 
   // It is always safe to copy over the NoWrap and FastMath flags. In
@@ -697,7 +734,8 @@ createWidenInductionRecipe(PHINode *Phi, VPPhi *PhiR, VPIRValue *Start,
   auto *WideIV = new VPWidenIntOrFpInductionRecipe(
       Phi, Start, Step, &Plan.getVF(), IndDesc, Flags, DL);
 
-  ReplaceExtractsWithExitingIVValue(WideIV);
+  if (!HasVariantStepIncrement)
+    ReplaceExtractsWithExitingIVValue(WideIV);
   return WideIV;
 }
 

retrochristian5000/LLVM

llvm/lib/Transforms/Vectorize/VPlanConstruction.cpp
@@ -615,6 +615,40 @@ static void simplifyLiveInsWithSCEV(VPlan &Plan,
 /// To make RUN_VPLAN_PASS print initial VPlan.
 static void printAfterInitialConstruction(VPlan &) {}
 
+/// Returns true if \p BackedgeVal advances \p PhiR by a loop-variant step.
+static bool hasVariantStepIncrement(VPValue *BackedgeVal, VPPhi *PhiR,
+                                    const InductionDescriptor &IndDesc,
+                                    PredicatedScalarEvolution &PSE,
+                                    Loop &OrigLoop) {
+  if (IndDesc.getKind() != InductionDescriptor::IK_IntInduction)
+    return false;
+
+  // Use m_c_Add to capture ActualStep regardless of operand order; subtraction
+  // is non-commutative so m_Sub requires PhiR on the left.
+  VPValue *ActualStep = nullptr;
+  switch (IndDesc.getInductionOpcode()) {
+  case Instruction::Add:
+    if (!match(BackedgeVal, m_c_Add(m_Specific(PhiR), m_VPValue(ActualStep))))
+      return false;
+    break;
+  case Instruction::Sub:
+    if (!match(BackedgeVal, m_Sub(m_Specific(PhiR), m_VPValue(ActualStep))))
+      return false;
+    break;
+  default:
+    return false;
+  }
+
+  const SCEV *ActualStepSCEV = vputils::getSCEVExprForVPValue(ActualStep, PSE);
+  // If the VPlan step has no computable SCEV, conservatively treat it as
+  // variant to avoid replacing it with the (potentially mismatched) IndDesc
+  // step or computing a wrong exit value.
+  if (isa<SCEVCouldNotCompute>(ActualStepSCEV))
+    return true;
+
+  return !PSE.getSE()->isLoopInvariant(ActualStepSCEV, &OrigLoop);
+}
+
 std::unique_ptr<VPlan>
 VPlanTransforms::buildVPlan0(Loop *TheLoop, LoopInfo &LI, Type *InductionTy,
                              PredicatedScalarEvolution &PSE,
@@ -700,7 +734,10 @@ createWidenInductionRecipe(PHINode *Phi, VPPhi *PhiR, VPIRValue *Start,
   // Update wide induction increments to use the same step as the corresponding
   // wide induction. This enables detecting induction increments directly in
   // VPlan and removes redundant splats.
-  if (match(BackedgeVal, m_Add(m_Specific(PhiR), m_VPValue())))
+  bool HasVariantStepIncrement =
+      hasVariantStepIncrement(BackedgeVal, PhiR, IndDesc, PSE, OrigLoop);
+  if (!HasVariantStepIncrement &&
+      match(BackedgeVal, m_Add(m_Specific(PhiR), m_VPValue())))
     BackedgeVal->getDefiningRecipe()->setOperand(1, Step);
 
   // It is always safe to copy over the NoWrap and FastMath flags. In
@@ -711,7 +748,8 @@ createWidenInductionRecipe(PHINode *Phi, VPPhi *PhiR, VPIRValue *Start,
   auto *WideIV = new VPWidenIntOrFpInductionRecipe(
       Phi, Start, Step, &Plan.getVF(), IndDesc, Flags, DL);
 
-  ReplaceExtractsWithExitingIVValueIfPossible(WideIV);
+  if (!HasVariantStepIncrement)
+    ReplaceExtractsWithExitingIVValueIfPossible(WideIV);
   return WideIV;
 }
 