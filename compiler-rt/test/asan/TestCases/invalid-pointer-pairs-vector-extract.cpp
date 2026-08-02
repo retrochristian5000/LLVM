203824610c536daa72512fefd51d9def76d38ff5
flovent <flbven@protonmail.com>
2026-08-02 21:53:35 +0800

    [NFC] clang-format test file

--- compiler-rt/test/asan/TestCases/invalid-pointer-pairs-vector-extract.cpp
+++ compiler-rt/test/asan/TestCases/invalid-pointer-pairs-vector-extract.cpp
@@ -3,9 +3,9 @@
 // RUN: %env_asan_opts=detect_invalid_pointer_pairs=1:halt_on_error=0 %run %t
 
 int main() {
-  const char* begins[2] = {"abc", "def"};
-  const char* ends[2] = {"abcde", "defgh"};
-  long lengths[2] {0};
+  const char *begins[2] = {"abc", "def"};
+  const char *ends[2] = {"abcde", "defgh"};
+  long lengths[2]{0};
   // CHECK: ERROR: AddressSanitizer: invalid-pointer-pair
   // CHECK: #{{[0-9]+ .*}} in main {{.*}}invalid-pointer-pairs-vector-extract.cpp:[[@LINE+1]]
   lengths[0] = ends[0] - begins[0];
