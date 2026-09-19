916bead69f5365ae646c9a773f910063c118515a
retrochristian5000 <252152985+retrochristian5000@users.noreply.github.com>
2026-09-19 13:53:52 -0400

    Instrumentation: drop redundant string conversion in sanitizer metadata

--- llvm/lib/Transforms/Instrumentation/SanitizerBinaryMetadata.cpp
+++ llvm/lib/Transforms/Instrumentation/SanitizerBinaryMetadata.cpp
@@ -213,7 +213,7 @@ bool SanitizerBinaryMetadata::run() {
     // Calls to the initialization functions with different versions cannot be
     // merged. Give the structors unique names based on the version, which will
     // also be used as the COMDAT key.
-    const std::string StructorPrefix = (MI->FunctionPrefix + VersionStr).str();
+    const std::string StructorPrefix = MI->FunctionPrefix + VersionStr;
 
     // We declare the _add and _del functions as weak, and only call them if
     // there is a valid symbol linked. This allows building binaries with
